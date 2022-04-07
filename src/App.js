311fed059e0f1fe6e5f7d59f3157a91a5e92fbb8
iamminho <minho100227@kakao.com>
2022-04-07 19:34:43 +0900

    useNavigate, useSearchParams 사용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
+import RouteTest from './components/RouteTest';
+
 import Home from './pages/Home'
 import New from './pages/New'
 import Edit from './pages/Edit'
@@ -15,8 +17,10 @@ function App() {
           <Route path="/" element = {<Home />} />
           <Route path="/new" element = {<New />} />
           <Route path="/edit" element = {<Edit />} />
+          <Route path="/diary/:id" element = {<Diary/>} />
           <Route path="/diary" element = {<Diary/>} />
         </Routes>
+        <RouteTest />
       </div>
     </Router>
   );    
