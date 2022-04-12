d6ae29469da7e40e93e8d925f0fd5b0b5ba5364d
iamminho <minho100227@kakao.com>
2022-04-12 15:35:59 +0900

    새 일기쓰기 navigate and 정렬버튼 css

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import MyButton from './MyButton';
+import { useNavigate } from 'react-router-dom';
 const sortOptionList = [
   {value:"latest", name: "최신순"},
   {value:"oldest", name: "오래된 순"},
@@ -12,9 +13,13 @@ const filterOptionList = [
 
 ]
 
-const ControlMenu = ({ value, onChange, optionList}) => {
+const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
       {optionList.map((it, idx) => (
         <option key={idx} value={it.value}>
           {it.name}
@@ -24,6 +29,7 @@ const ControlMenu = ({ value, onChange, optionList}) => {
   );
 };
 const DiaryList = ({ diaryList }) => {  
+  const navigate = useNavigate();
   console.log(diaryList);
   const [sortType, setSortType] = useState("lastest");
   const [filter,setFilter] = useState("all");
@@ -54,22 +60,36 @@ const DiaryList = ({ diaryList }) => {
   
   // console.log(getProcessdDiaryList());
   return (
-    <div>
-      <ControlMenu
-        value={sortType}
-        onChange={setSortType}
-        optionList={sortOptionList}
-      />
-      <ControlMenu
-        value={filter}
-        onChange={setFilter}
-        optionList={filterOptionList}
-      />
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
+        </div>
+        <div className="right_col">
+          <MyButton
+            type={"success"}
+            text={"새 일기쓰기"}
+            onClick={() => navigate("/new")}
+          />
+        </div>
+      </div>
+
       {/* getProcessdDiaryList~ 부분을 주석처리 했다가 다시 주석해제하면 에러가 없어진다? */}
       {/* 문제: 갑자기 diaryList를 배열로 인식 못함 */}
-      {getProcessdDiaryList().map((it) => (
-        <div key={it.id}>{it.content} {it.emotion}</div>
-      ))}
+      {/* {getProcessdDiaryList().map((it) => (
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
+      ))} */}
     </div>
   );
 };
