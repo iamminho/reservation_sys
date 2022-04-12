2dc2da3222b07f85bdcda89c12004109d4e2dca0
iamminho <minho100227@kakao.com>
2022-04-12 17:55:58 +0900

    상세페이지, 수정페이지 navigate

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import MyButton from './MyButton';
+import DiaryItem from './DiaryItem';
 import { useNavigate } from 'react-router-dom';
 const sortOptionList = [
   {value:"latest", name: "최신순"},
@@ -85,11 +86,12 @@ const DiaryList = ({ diaryList }) => {
 
       {/* getProcessdDiaryList~ 부분을 주석처리 했다가 다시 주석해제하면 에러가 없어진다? */}
       {/* 문제: 갑자기 diaryList를 배열로 인식 못함 */}
-      {/* {getProcessdDiaryList().map((it) => (
-        <div key={it.id}>
-          {it.content} {it.emotion}
-        </div>
-      ))} */}
+      {getProcessdDiaryList().map((it) => (
+        <DiaryItem key={it.id}
+          {...it}
+        />
+       
+      ))}
     </div>
   );
 };
